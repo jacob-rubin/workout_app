f460ea2ddb665276510bb473c8a773910b46db4e
Jacob-Rubin-VTNC34_moto <jacob.rubin@motorolasolutions.com>
2024-01-20 11:21:38 -0600

    Fix tests

--- test/services/load_exercises_test.dart
+++ test/services/load_exercises_test.dart
@@ -34,11 +34,12 @@ void main() {
       final List<Exercise> dbExercises = await mockIsar.exercises.where().findAll();
 
       for (var i = 0; i < exerciseJSON.length; i++) {
-        expect(dbExercises[i].name, exerciseJSON[i]['name']);
-        expect(dbExercises[i].bodyPart, exerciseJSON[i]['bodyPart']);
-        expect(dbExercises[i].equipment, exerciseJSON[i]['equipment']);
-        expect(dbExercises[i].targetMuscle, exerciseJSON[i]['target']);
-        expect(dbExercises[i].secondaryMuscles, exerciseJSON[i]['secondaryMuscles'].cast<String>());
+        expect(dbExercises[i].name.toLowerCase(), exerciseJSON[i]['name'].toLowerCase());
+        expect(dbExercises[i].bodyPart.toLowerCase(), exerciseJSON[i]['bodyPart'].toLowerCase());
+        expect(dbExercises[i].equipment.toLowerCase(), exerciseJSON[i]['equipment'].toLowerCase());
+        expect(dbExercises[i].targetMuscle.toLowerCase(), exerciseJSON[i]['target'].toLowerCase());
+        expect(dbExercises[i].secondaryMuscles.map((e) => e.toLowerCase()),
+            exerciseJSON[i]['secondaryMuscles'].cast<String>().map((e) => e.toLowerCase()));
         expect(dbExercises[i].instructions, exerciseJSON[i]['instructions'].cast<String>());
         expect(dbExercises[i].gifId, exerciseJSON[i]['id']);
       }
