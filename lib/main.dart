ff2ba83c75d33304ed87b9aee04152f86e1a8101
Jacob Rubin <jacob.rubin18@gmail.com>
2024-06-09 09:57:07 -0500

    Initialize firebase in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:workout_app/database/providers/database_provider.dart';
@@ -5,7 +6,16 @@ import 'package:workout_app/database/providers/exercise_search_provider.dart';
 import 'package:workout_app/database/providers/tab_provider.dart';
 import 'package:workout_app/screens/home/home.dart';
 
-void main() => runApp(const MyApp());
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
